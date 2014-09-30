17386fda112a1472fa9c9e319847593f5cd56dac
smesh119 <smesh119@gmail.com>
2014-09-30 18:47:57 -0400

    updates to query

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 	$(".query").keydown(function(e){
 		if(e.which == 13){
-			$(".list").appendTo("<li>")
-		}
-	})
+			var toAdd = $("input[name=checklistitem]").val();
+			$(".list").append("<li class="added">" + toAdd + "</li>");
+	});
+			$(document).on("click", ".added", function(){
+				$(this).remove();
+			
+	});
 
 });
