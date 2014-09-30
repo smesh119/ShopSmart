c15315c082400ea8d5e529b99e46cc41c2e80216
smesh119 <smesh119@gmail.com>
2014-09-30 18:49:46 -0400

    update

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
 		if(e.which == 13){
 			var toAdd = $("input[name=checklistitem]").val();
 			$(".list").append("<li class="added">" + toAdd + "</li>");
-	});
-			$(document).on("click", ".added", function(){
+				$(document).on("click", ".added", function(){
 				$(this).remove();
 			
 	});
