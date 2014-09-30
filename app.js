33b6b2ad04e80cd75ed096bf80c254c62cf5d9ea
smesh119 <smesh119@gmail.com>
2014-09-30 18:57:14 -0400

    added li into input

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	$(".query").keydown(function(e){
 		if(e.which == 13){
-			var toAdd = $("input[name=checklistitem]").val();
-			$(".list").append("<li class="added">" + toAdd + "</li>");
+			var toAdd = $("input class="query",[name=checklistitem]").val();
+			$(".list").append("<li class="added"><input type="checkbox" name="item"  value="1">
+              <span class="on-list">" + toAdd + "</span></li>");
 				$(document).on("click", ".added", function(){
 				$(this).remove();
 			
