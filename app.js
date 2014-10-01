ef2aa0db9481c71c6f4d632e9efe1be6f54c9f1c
smesh119 <smesh119@gmail.com>
2014-09-30 22:27:19 -0400

    grrrr

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 	$(".query").keydown(function(e){
 		if(e.which == 13){
-			var toAdd = $("input class="query",[name=checklistitem]").val();
-			$(".list").append("<li class="added"><input type="checkbox" name="item"  value="1">
-              <span class="on-list">" + toAdd + "</span></li>");
-				$(document).on("click", ".added", function(){
-				$(this).remove();
-			
+			var toAdd = $("input[name=checklistitem]").val();
+			console.log(toAdd)
+			$(".list").append("<li class=\"added\"><input type=\"checkbox\" name=\"item\"  value=\"1\">\
+              <span class=\"on-list\">" + toAdd + "</span></li>");
+		}
 	});
+		$(document.body).delegate(".list li", "click", function(event){
+		$(this).remove()
+		});
+		
+		var target = $(event.target);
+		if (target.is("input[name=item]")) {
+			target.stop();
+		};
+	
 
 });
