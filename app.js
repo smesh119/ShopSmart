32ffd64ad870917d7bbca60c4be1c4fdd5ab1191
smesh119 <smesh119@gmail.com>
2014-10-01 20:53:46 -0400

    delegation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	
 	/* INPUT SECTION on keydown*/
 	$(".query").keydown(function(e){
 		if(e.which == 13){
@@ -7,17 +8,21 @@ $(document).ready(function() {
 			console.log(toAdd)
 			$(".list").append("<li class=\"added\"><span class=\"on-list\">" + toAdd + "</span><img class=\"delete\" src=\"delete.gif\"></li>");
 		}
+
 	});
+
+		/*$(".query").keyup(function(){
+			$("toAdd").remove();
+		});/*
 
 		/* CLICK LIST ITEM TO cross out*/
 		$(document.body).delegate(".list .on-list", "click", function(event){
 		$(this).css("text-decoration", "line-through");
 		});
 		
-		$(".delete").click(function(){
+		$(document.body).delegate(".delete", "click", function(){
 			$(this).parent().remove();
 		});
 
-
-
+		
 });
