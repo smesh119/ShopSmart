b08dca7213a5390c45b2c3ccc5a30e2697df89be
smesh119 <smesh119@gmail.com>
2014-10-01 21:38:08 -0400

    Add toggle class to line through

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
 
-	
 	/* INPUT SECTION on keydown*/
 	$(".query").keydown(function(e){
 		if(e.which == 13){
 			var toAdd = $("input[name=checklistitem]").val();
 			console.log(toAdd)
 			$(".list").append("<li class=\"added\"><span class=\"on-list\">" + toAdd + "</span><img class=\"delete\" src=\"delete.gif\"></li>");
-		}
-
+		
+}	
 	});
 
-		/*$(".query").keyup(function(){
-			$("toAdd").remove();
-		});/*
+	$(".query").on("focus", function(e){
+        $(".query").val("")
+    });
 
+    $(".query").keyup(function(e){
+        if (e.which == 13) {
+        	$(".query").val("")
+        };
+    });
+
+	/*$(".test").sortable({ cursor: "move" });*/
+
+		
 		/* CLICK LIST ITEM TO cross out*/
 		$(document.body).delegate(".list .on-list", "click", function(event){
-		$(this).css("text-decoration", "line-through");
+		$(this).toggleClass("highlight");
 		});
 		
 		$(document.body).delegate(".delete", "click", function(){
