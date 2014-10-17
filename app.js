3f0e456871310cb40181e5c8834620f2598fd479
smesh119 <smesh119@gmail.com>
2014-10-16 21:04:57 -0400

    upate

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 $(document).ready(function() {
 
-	/* INPUT SECTION on keydown*/
-	$(".query").keydown(function(e){
-		if(e.which == 13){
-			var toAdd = $("input[name=checklistitem]").val();
-			console.log(toAdd)
-			$(".list").append("<li class=\"added\"><span class=\"on-list\">" + toAdd + "</span><img class=\"delete\" src=\"delete.gif\"></li>");
-		
-}	
-	});
+$(".query").keydown(function(e){
+	if(e.which == 13){
+	var toAdd = $("input[name=checklistitem]").val();
+	console.log(toAdd)
+	$(".list").append("<li class=\"added\"><span class=\"on-list\"> " + toAdd + "</span>
+  	<img class=\"delete\" src=\"delete.gif\">
+	</li>");	
+	$("ul").sortable("refresh");
+	$( "ul" ).disableSelection();
 
-	$(".query").on("focus", function(e){
-        $(".query").val("")
-    });
+	}	
+});
 
-    $(".query").keyup(function(e){
-        if (e.which == 13) {
-        	$(".query").val("")
-        };
-    });
+$(".query").on("focus", function(e){
+$(".query").val("")
+});
 
-	/*$(".test").sortable({ cursor: "move" });*/
-	$(function() {
-    $( "ul" ).sortable();
-    $( "ul" ).disableSelection();
+$(".query").keyup(function(e){
+if (e.which == 13) {
+$(".query").val("")
+};
+});
+/*
+$(function() {
+$( "ul" ).sortable();
+$( "ul" ).disableSelection();
 
-  });
-		
-		/* CLICK LIST ITEM TO cross out*/
-		$(document.body).delegate(".list .on-list", "click", function(event){
-		$(this).toggleClass("highlight");
-		});
-		
-		$(document.body).delegate(".delete", "click", function(){
-			$(this).parent().remove();
-		});
+});
+*/
 
-		
+$(document.body).delegate(".list .on-list", "click", function(event){
+$(this).toggleClass("highlight");
+});
+
+$(document.body).delegate(".delete", "click", function(){
+$(this).parent().remove();
+});
+
+
 });
