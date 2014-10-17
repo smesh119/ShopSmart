362b95c00a8262a8b5e19ecb7b95b7f35e823d6a
smesh119 <smesh119@gmail.com>
2014-10-16 21:07:03 -0400

    update

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ $(".query").keydown(function(e){
 	$(".list").append("<li class=\"added\"><span class=\"on-list\"> " + toAdd + "</span>
   	<img class=\"delete\" src=\"delete.gif\">
 	</li>");	
-	$("ul").sortable("refresh");
-	$( "ul" ).disableSelection();
+	toAdd.sortable("refresh");
+	toAdd.disableSelection();
 
 	}	
 });
